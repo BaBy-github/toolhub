d03028e6cec48f9eaa6791ae46a86c804fbd11bb
BaBy <614645323@qq.com>
2025-12-10 23:18:35 +0800

    style(Home): 调整工具区块最小高度为视窗高度减去200px

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -57,7 +57,7 @@ const getColorValue = (color: string, shade: number) => {
     </section>
 
     <!-- Tools Section -->
-    <section id="tools" class="py-16">
+    <section id="tools" class="py-16 min-h-[calc(100vh-200px)]">
       <div class="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-3">
         <router-link 
           v-for="tool in tools" 
